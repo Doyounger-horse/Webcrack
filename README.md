fda9ab2bd648efa807df7075d05bbd1f584c97c3
Adrien Vasseur <adrien.vasseur@epitech.eu>
2015-10-02 10:35:49 +0200

    First commit : compilation instructions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Webcrack
+
+## Building instructions
+
+To build the project :
+
+1. `qmake` in the root directory ;
+2. `make` in the root directory ;
+3. Run Webcrack in the root directory ;
+
+Required :
+
+1. Qt >= 5.0
---
+++ src/webcrack.cpp
@@ -0,0 +1,18 @@
+/*
+** webcrack.cpp for Webcrack in /home/vasseu_g/github/Webcrack/src
+** 
+** Made by Adrien Vasseur
+** Login   <vasseu_g@epitech.net>
+** 
+** Started on  Fri Oct  2 10:26:10 2015 Adrien Vasseur
+** Last update Fri Oct  2 10:28:39 2015 Adrien Vasseur
+*/
+
+#include	<QApplication>
+
+int		main(int argc, char **argv)
+{
+  QApplication	app(argc, argv);
+
+  return (app.exec());
+}
